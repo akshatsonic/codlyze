84bdd2aca68586a75ae7b7320439d1f7f789f45d
akshatsonic <akshatsoni1529@gmail.com>
2024-10-29 09:48:27 +0530

    Initial Commit

---
+++ src/main/java/com/akshatsonic/codelyze/CodelyzeApplication.java
@@ -0,0 +1,13 @@
+package com.akshatsonic.codelyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodelyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelyzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshatsonic/codelyze/CodelyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshatsonic.codelyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelyzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
